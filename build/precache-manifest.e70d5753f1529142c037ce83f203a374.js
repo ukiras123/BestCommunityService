a7f6ebe424a41c4d006c8e869075612d5474998b
“ukiras123” <ukiras@gmail.com>
2019-02-19 12:48:39 -0800

    adding build

---
+++ build/precache-manifest.e70d5753f1529142c037ce83f203a374.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b58784575b578c6bd77b",
+    "url": "/static/js/main.b5878457.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0acd1b8a95e28ef8d94e",
+    "url": "/static/css/2.3eaef360.chunk.css"
+  },
+  {
+    "revision": "0acd1b8a95e28ef8d94e",
+    "url": "/static/js/2.0acd1b8a.chunk.js"
+  },
+  {
+    "revision": "642008c19bb4de05965b8db054e10dc7",
+    "url": "/static/media/catering.642008c1.jpg"
+  },
+  {
+    "revision": "8d075fad39fe1d358b3979c4771c0fd4",
+    "url": "/static/media/classes.8d075fad.jpg"
+  },
+  {
+    "revision": "93719e90b447fc2e671e32f94c7823b5",
+    "url": "/static/media/equipments.93719e90.jpg"
+  },
+  {
+    "revision": "c7ea2fd3e0c056c5cfc135e4f9a5bf0e",
+    "url": "/static/media/hall.c7ea2fd3.jpg"
+  },
+  {
+    "revision": "af0cb7339defe7286593972a4267228e",
+    "url": "/static/media/homecare.af0cb733.jpg"
+  },
+  {
+    "revision": "1ba422b0d81210ed423c87da2b0e3ef1",
+    "url": "/static/media/shuttle.1ba422b0.jpg"
+  },
+  {
+    "revision": "545e4ba1dea750d2375e5f135080278e",
+    "url": "/index.html"
+  }
+];
